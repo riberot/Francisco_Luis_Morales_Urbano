5c8e87ba51c4eae5dc7f35ad94db4250924a9fe3
riberot <francisriberot@gmail.com>
2013-05-09 16:10:16 +0200

    primer commit

---
+++ Hola_Mundo/src/Hola_Mundo.java
@@ -0,0 +1,12 @@
+public class Hola_Mundo {
+
+	/**
+	 * Programa inicial hola mundo
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
